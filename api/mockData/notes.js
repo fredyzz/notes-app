a251f5fe310c708675577bc3ec6d614e46083e6e
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-08-22 14:11:04 +0200

    Create mockData/notes

---
+++ api/mockData/notes.js
@@ -0,0 +1,24 @@
+const NOTES =  [
+    {
+        "id": 1,
+        "content": "Nota 1",
+        "date": "2021-08-10T19:20:14.298Z",
+        "important": true
+    },
+    {
+        "id": 2,
+        "content": "Nota 2",
+        "date": "2021-10-10T19:20:14.298Z",
+        "important": false
+    },
+    {
+        "id": 3,
+        "content": "Nota 3",
+        "date": "2021-11-10T19:20:14.298Z",
+        "important": true
+    }
+]
+
+module.exports = {
+    NOTES: NOTES
+}
